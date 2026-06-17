f7c8e2227df6947b513bab6a5f1af0e4dce73d64
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-06-17 23:17:36 +0530

    Addes Home Page

---
+++ Fronthend/vite-project/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import StudentList from "../Components/StudentList";
+import Navbar from "../Components/NavBar";
+function Home() {
+    return (
+
+        <>
+         <Navbar/>
+         <StudentList/>
+        </>
+    )
+}
+
+export default Home;
