830ffc918255c37238eddc0ca9e4273b14dd8529
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-06-17 23:18:46 +0530

    Added React Router Dom

--- Fronthend/vite-project/src/App.jsx
+++ Fronthend/vite-project/src/App.jsx
@@ -1,30 +1,33 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import axios from "axios"
-import StudentList from './Components/StudentList'
-import StudentSerch from './Components/StudentSerch'
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Home from "./Pages/Home"
+import AddStudent from "./Pages/AddStudent";
+import StudentSerch from "./Pages/StudentSerch";
+import Navbar from "./Components/NavBar";
 
-function App() {
-  const [count, setCount] = useState(0)
-  useEffect(()=>{
-    axios.get("http://127.0.0.1:8000/students")
-    .then((Response)=>{
-      console.log(Response.data)
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
-  },[])
+function App(){
 
   return (
-    <>
-    <StudentSerch/>
-      <StudentList/>
-    </>
+    
+    <div>
+     
+    
+      <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home/>}/>
+          <Route
+          path="/add-student"
+          element={<AddStudent />}
+        />
+
+         <Route
+          path="/search-student"
+          element={<StudentSerch  />}
+        />
+      </Routes>
+    
+    </BrowserRouter>
+    </div>
   )
 }
-
-export default App
+export default App;
