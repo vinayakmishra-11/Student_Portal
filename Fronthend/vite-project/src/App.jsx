00628afd8296bd06f4c3bcfa1fda255f4988e57c
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-06-23 23:12:29 +0530

    Added Page link of Updating Student

--- Fronthend/vite-project/src/App.jsx
+++ Fronthend/vite-project/src/App.jsx
@@ -1,36 +1,24 @@
-import {BrowserRouter , Routes , Route} from "react-router-dom";
-import Home from "./Pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import AddStudent from "./Pages/AddStudent";
 import StudentSerch from "./Pages/StudentSerch";
 import Navbar from "./Components/NavBar";
 
-function App(){
-   
+function App() {
   return (
-    
     <div>
-     
-       
       <BrowserRouter>
-      {/* <Navbar/> */}
-       <Navbar/>
-      <Routes>
-      
-        <Route path="/" element={<Home/>}/>
-          <Route
-          path="/add-student"
-          element={<AddStudent />}
-        />
+        {/* <Navbar/> */}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-student" element={<AddStudent />} />
+          <Route path="/edit-student/:id" element={<UpdateStudent />} />
 
-         <Route
-          path="/search-student"
-          element={<StudentSerch  />}
-        />
-      </Routes>
-             
-    
-    </BrowserRouter>
+          <Route path="/search-student" element={<StudentSerch />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 export default App;
