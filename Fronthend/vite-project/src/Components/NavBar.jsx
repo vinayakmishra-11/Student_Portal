ba61deacfa41b759dcf050c41e1586cefac3f9d5
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-06-28 22:08:39 +0530

    NAvBAR added link to NvbAr HEadings

--- Fronthend/vite-project/src/Components/NavBar.jsx
+++ Fronthend/vite-project/src/Components/NavBar.jsx
@@ -1,23 +1,26 @@
-import {Link} from "react-router-dom";
-import '../style/NavBar.css';
-
+import { Link } from "react-router-dom";
+import "../style/NavBar.css";
+import { useNavigate, useParams } from "react-router-dom";
 function Navbar() {
-    return (
-        <nav className="app-navbar">
-            <div className="app-navbar__brand">Student Details</div>
-            <div className="app-navbar__links">
-                <Link className="app-navbar__link" to="/">
-                    Student List
-                </Link>
-                <Link className="app-navbar__link" to="/add-student">
-                    Add Student
-                </Link>
-                <Link className="app-navbar__link" to="/search-student">
-                    Search Student
-                </Link>
-            </div>
-        </nav>
-    )
+  const navigate = useNavigate();
+  return (
+    <nav className="app-navbar">
+      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+        <div className="app-navbar__brand">Student Details</div>
+      </Link>
+      <div className="app-navbar__links">
+        <Link className="app-navbar__link" to="/">
+          Student List
+        </Link>
+        <Link className="app-navbar__link" to="/add-student">
+          Add Student
+        </Link>
+        <Link className="app-navbar__link" to="/search-student">
+          Search Student
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
